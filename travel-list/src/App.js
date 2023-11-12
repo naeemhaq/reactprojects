5cfa456650993ba8ce79b42b69c301c23e41b600
Naeem Ul-haq <naeemhaq@gmail.com>
2023-11-12 07:54:59 -0500

    delete item, child to parent communication

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -7,17 +7,30 @@ const initialItems = [
 ];
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  //const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
+  }
+  /*
+    In order to delete an item, we need to know which item it actually is that should be deleted. So to tell the function which item it is, handleDelete(id).
+    Each of the items has an ID, so we use that ID to remove the corresponding object from the items array. Now about the delete operation itself, we will of 
+    course delete the item from the user interface by updating state. So we call setItems(). And now here in setItems(), we need the new array after the item 
+    has been deleted. The new Array will be based on the current/existing one therefore we pass current items to a callback function with current item as its 
+    input. We want to filter out the item that has the parameter ID items.filter(item=>item.id !==id) i.e. in each iteration we look for item.id which is 
+    different from ID. So whenever this condition here is true, the item will end up in the new array i.e. items not deleted. And if false item will no longer 
+    be part of final array.
+  */
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
   }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -67,25 +80,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
