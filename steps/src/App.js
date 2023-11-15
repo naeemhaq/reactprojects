b45efd588a5469354231552892df6aa3160b34ba
Naeem Ul-haq <naeemhaq@gmail.com>
2023-11-15 03:19:35 -0500

    reusable components children props

--- steps/src/App.js
+++ steps/src/App.js
@@ -8,6 +8,10 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
     </div>
   );
 }
@@ -37,7 +41,18 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn Children
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrvious}>
@@ -55,11 +70,9 @@ function Steps() {
 
 function StepMessage({ step, children }) {
   return (
-    <div>
-      <p className="message">
-        <h3>Step {step}: </h3>
-        {children}
-      </p>
+    <div className="message">
+      <h3>Step {step}: </h3>
+      {children}
     </div>
   );
 }
