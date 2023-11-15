2bcd4bf8488d895df5d9d1898d44bf890e94c339
Naeem Ul-haq <naeemhaq@gmail.com>
2023-11-15 02:10:56 -0500

    creating components with childern props

--- steps/src/App.js
+++ steps/src/App.js
@@ -37,10 +37,7 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrvious}>
@@ -52,6 +49,17 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div>
+      <p className="message">
+        <h3>Step {step}: </h3>
+        {children}
+      </p>
     </div>
   );
 }
