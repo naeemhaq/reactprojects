f01d4225343e4197bc3336e2ddbb458138eec037
Naeem Ul-haq <naeemhaq@gmail.com>
2023-12-13 02:22:59 -0500

    add another custom hook

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 const average = (arr) =>
   arr?.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -67,11 +68,7 @@ export default function App() {
   const [selectedId, setSelectedId] = useState("");
   const { movies, isLoading, error } = useMovies(query);
 
-  // const [watched, setWatched] = useState([]);
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched");
-    return storedValue ? JSON.parse(storedValue) : [];
-  });
+  const [watched, setWatched] = useLocalStorageState([], "watched");
 
   function handleSelectedMovie(Id) {
     setSelectedId((selectedId) => (Id === selectedId ? null : Id));
